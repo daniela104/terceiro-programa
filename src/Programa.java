b661b9e5aa6fbf54c087e2f1261ea8069a91dd72
daniela104 <dani_ela090@hotmail.com>
2020-11-29 09:04:22 -0300

    Adicionando as condicoes com if

--- src/Programa.java
+++ src/Programa.java
@@ -1,7 +1,32 @@
 public class Programa{
 	public static void main(String[] args){
+	
+		System.out.println();
+		System.out.println();
 		
+		String idadeDigitada = args[0];
+		System.out.println("A idade é " + idadeDigitada);
+		int idade = Integer.parseInt(idadeDigitada);
+		System.out.println("A idade como inteiro é " + idade);
 		
-		
+		System.out.println();
+		System.out.println();
+
+		if (idade <18){
+			System.out.println("pode entrar acompanhado");
+		}else if (idade>=18 && idade <=60){
+			System.out.println("pode entrar");
+		}else{	
+			System.out.println("não pode entrar");
+			
 		}
+		
+	}
 }
+
+
+
+
+
+
+
